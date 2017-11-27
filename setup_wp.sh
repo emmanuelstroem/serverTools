e5aba39acd3d776d894aaf0fe34a13201ad1ba2f
Emmanuel <emmanuel.stroem@gmail.com>
2017-11-27 17:08:27 +0800

    fixed mysql cnf file location

--- setup_wp.sh
+++ setup_wp.sh
@@ -138,12 +138,14 @@ groups www-data
 echo "======= Installing debconf-utils ============"
 apt-get install -y debconf-utils
 
+apt-get -y install zsh htop
+
 # Install MySQL
 echo "======= Setting MySQL default root password ============"
 # echo 'mysql-server mysql-server/root_password password password secret' | debconf-set-selections
 # echo 'mysql-server mysql-server/root_password_again password password secret' | debconf-set-selections
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password secret"
-sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password secret"
+echo "mysql-server mysql-server/root_password password secret" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password secret" | sudo debconf-set-selections
 
 # Update the information needed for APT by adding the 5.7 repository and updating `apt-get
 
@@ -156,7 +158,14 @@ echo "======= Installing MySQL ============"
 apt-get install -y mysql-server-5.7
 
 # Secure MySQL Install
-# echo "======= Running mysql_secure_installation ============"
+echo "======= Running mysql_secure_installation ============"
+# Run the MySQL Secure Installation wizard
+# mysql_secure_installation
+echo -e "secret\nn\nY\nY\nY\nY\n" | mysql_secure_installation
+
+sed -i 's/127\.0\.0\.1/0\.0\.0\.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+mysql -uroot -p -e 'USE mysql; UPDATE `user` SET `Host`="%" WHERE `User`="root" AND `Host`="localhost"; DELETE FROM `user` WHERE `Host` != "%" AND `User`="root"; FLUSH PRIVILEGES;'
+
 # sudo mysql_secure_installation
 
 # Configure MySQL Password Lifetime
