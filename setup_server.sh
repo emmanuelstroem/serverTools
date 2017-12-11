a23eeff36fe66533fc957c093a9d185f3e3d1ae3
Emmanuel <emmanuel.stroem@gmail.com>
2017-12-12 00:27:10 +0800

    added proper commentary

--- setup_server.sh
+++ setup_server.sh
@@ -7,10 +7,15 @@ sudo DEBIAN_FRONTEND=noninteractive
 
 # Security measures for SSH
 #
+echo "=============== - Change ssh port to 20022 ==============="
 # - Change ssh port to 20022
 sed -i 's/Port 22/Port 20022/' /etc/ssh/sshd_config
+
+echo "================ - Disallow root login ==============="
 # - disallow root login
 sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+
+echo "================ - Disallow Password Auth ==============="
 # - disallow password auth
 cat >> /etc/ssh/sshd_config << EOF
 PasswordAuthentication no
@@ -18,16 +23,21 @@ AllowUsers manu
 EOF
 
 # Add user etc
+echo "================ - Add User (manu) ==============="
 # - add manu user & set base permissions
 echo "manu:manu:1000:1000::/home/manu:/bin/bash" | newusers
 cp -a /etc/skel/.[a-z]* /home/manu/
 chown -R manu:www-data /home/manu/
 
+echo "================ - Add User to SUDOERS ==============="
 # add user to sudoers
 usermod -aG sudo manu
 
+echo "================ - Add sudo ability to User ==============="
 # - add sudo ability
-echo "manu    ALL=NOPASSWD: ALL" >> /etc/sudoers
+echo "manu   ALL=NOPASSWD: ALL" >> /etc/sudoers
+
+echo "================ - Configure SSH Keys ==============="
 # - configure SSH allowed keys
 mkdir /home/manu/.ssh
 chmod -R 600 /home/manu/.ssh/
@@ -35,6 +45,8 @@ touch /home/manu/.ssh/authorized_keys
 cat > /home/manu/.ssh/authorized_keys << EOF
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5BhQjg6WaR+0XDWPIXl8CErceSyGMhTUbZak1fA5wwvi8j2ax5CbF1CyWl7Rfd/tmz4lkPClrvhwgQiVNQ7T34en4ze1p9bZdh0BWdU2H2Cng6aUkWT+qQJNgiYR1que+ItTQcOzdzBZ2JCwxQsz1WRLSbdlnc3gXhjuGC4J2+TQk91osMcSONn/HuMRvpEGYUX+bZkatoxvwJx3Vtcl1awYHVOKa9c0js1QfNll3eUj35VUdwAIPaME56rD4QY6n3Io6iAN+yMG2bLayxEFoOIX+gkTxyw/UoMV0DW9DQmarvejTVjn8dnvnf8O7rwQLJuv2crNR1tm5f07i9QMz emmanuel@Emmanuels-MacBook-Pro.local
 EOF
+
+echo "================ - Set correct SSH file Permissions ==============="
 # - set correct SSH file permissions
 /etc/init.d/ssh restart
 chown -R manu /home/manu/
@@ -43,32 +55,30 @@ chmod 600 /home/manu/.ssh/authorized_keys
 
 # Systems stuff
 
+echo "================ - Install aptitude ==============="
 # 16.04 onwards doesn't have aptitude by default
 apt-get install -y aptitude
 
+echo "================ - Update System Packages ==============="
 # - upgrade all existing packages
-
-aptitude update
+apt update && apt upgrade
 # aptitude safe-upgrade -y
 
+echo "================ - Install htop iftop iotop sysstat screen curl ntp ==============="
 # - install base packages
+apt-get install -y htop iftop iotop sysstat screen curl ntp
 
-aptitude install htop iftop iotop sysstat screen curl ntp -y
-
+echo "================ - Enable Sysstat ==============="
 # - enable sysstat
-
 sed -i "s/ENABLED=\"false\"/ENABLED=\"true\"/" /etc/default/sysstat
 
-# - remove swap
-
-# swapoff -a
-# sed -i '/swap/d' /etc/fstab
-
+echo "================ - Add Secure User Password ==============="
 # - add secure manu password
-echo -e 'manuL1n0de2017!' | sudo passwd manu
+# echo -e 'manuL1n0de2017!' | sudo passwd manu
 
 # The end - remove script
-rm /root/install.sh
+# rm /root/install.sh
 
+echo "================ - REBOOT ==============="
 # Reboot - kinda obvious no?
-reboot
+reboot now
