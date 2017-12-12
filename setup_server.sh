732b8ae3f83c3e710d6ba145075e8d419996bad2
Emmanuel <emmanuel.stroem@gmail.com>
2017-12-12 18:11:15 +0800

    made major changes to commands

--- setup_server.sh
+++ setup_server.sh
@@ -5,6 +5,23 @@
 #
 export DEBIAN_FRONTEND="noninteractive"
 
+# command line arguments
+if [ $# -gt 0 ]; then
+    echo "Your command line contains $# arguments"
+    username="$1"
+    password="$2"
+else
+    echo "Your command line contains no arguments"
+		username="manu"
+    password=".manu@L1n0de2017!"
+fi
+
+echo "======= Got Domain Name ============"
+echo $username
+echo $password
+
+
+
 # Security measures for SSH
 #
 echo "=============== - Change ssh port to 20022 ==============="
@@ -17,41 +34,52 @@ sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 
 echo "================ - Disallow Password Auth ==============="
 # - disallow password auth
+sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+
 cat >> /etc/ssh/sshd_config << EOF
-PasswordAuthentication no
-AllowUsers manu
+AllowUsers $username
 EOF
 
-# Add user etc
-echo "================ - Add User (manu) ==============="
-# - add manu user & set base permissions
-echo "manu:manu:1000:1000::/home/manu:/bin/bash" | newusers
-cp -a /etc/skel/.[a-z]* /home/manu/
-chown -R manu:www-data /home/manu/
 
-echo "================ - Add User to SUDOERS ==============="
-# add user to sudoers
-usermod -aG sudo manu
+echo "================ CREATE USER ==============="
 
-echo "================ - Add sudo ability to User ==============="
-# - add sudo ability
-echo "manu   ALL=NOPASSWD: ALL" >> /etc/sudoers
+# echo " ************* - Add sudo ability to User "
+# # - add sudo ability
+# echo "manu   ALL=NOPASSWD: ALL" >> /etc/sudoers
 
-echo "================ - Configure SSH Keys ==============="
+echo " ************* - Configure SSH Keys ==============="
 # - configure SSH allowed keys
-mkdir /home/manu/.ssh
-chmod -R 600 /home/manu/.ssh/
-touch /home/manu/.ssh/authorized_keys
-cat > /home/manu/.ssh/authorized_keys << EOF
+echo " ************* - Create Home Directory + .ssh Directory ****************"
+mkdir -p /home/$username/.ssh
+
+echo " ************* - Create Authorized Keys File ****************"
+touch /home/$username/.ssh/authorized_keys
+
+echo " ************* - Add SSH Keys ****************"
+cat > /home/$username/.ssh/authorized_keys << EOF
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5BhQjg6WaR+0XDWPIXl8CErceSyGMhTUbZak1fA5wwvi8j2ax5CbF1CyWl7Rfd/tmz4lkPClrvhwgQiVNQ7T34en4ze1p9bZdh0BWdU2H2Cng6aUkWT+qQJNgiYR1que+ItTQcOzdzBZ2JCwxQsz1WRLSbdlnc3gXhjuGC4J2+TQk91osMcSONn/HuMRvpEGYUX+bZkatoxvwJx3Vtcl1awYHVOKa9c0js1QfNll3eUj35VUdwAIPaME56rD4QY6n3Io6iAN+yMG2bLayxEFoOIX+gkTxyw/UoMV0DW9DQmarvejTVjn8dnvnf8O7rwQLJuv2crNR1tm5f07i9QMz emmanuel@Emmanuels-MacBook-Pro.local
 EOF
 
-echo "================ - Set correct SSH file Permissions ==============="
-# - set correct SSH file permissions
-/etc/init.d/ssh restart
-chown -R manu /home/manu/
-chmod 700 /home/manu/.ssh
-chmod 600 /home/manu/.ssh/authorized_keys
+echo " ************* - Create User + Set Home Directory ****************"
+useradd -d /home/$username $username
+
+echo " ************* - Add User to sudo Group ****************"
+usermod -aG sudo $username
+
+echo " ************* - $username chown -R  Permissions on Home Directory ****************"
+chown -R $username:$username /home/$username/
+
+echo " ************* - ROOT chown Permissions on Home Directory ****************"
+chown root:root /home/$username
+
+echo " ************* - Set Permissions on .ssh Directory to 700 ****************"
+chmod 700 /home/$username/.ssh
+
+echo " ************* - Set Permissions on Authorized Key File to 0644 ****************"
+chmod 644 /home/$username/.ssh/authorized_keys
+
+echo " ************* - Set Password for User $username ****************"
+echo -e "$password\n$password\n" | sudo passwd $username
 
 # Systems stuff
 
@@ -61,24 +89,29 @@ apt-get install -y aptitude
 
 echo "================ - Update System Packages ==============="
 # - upgrade all existing packages
-apt update && apt upgrade -y
-# aptitude safe-upgrade -y
+aptitude update
+# aptitude update && aptitude --assume-yes safe-upgrade -y
+aptitude -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" upgrade
 
 echo "================ - Install htop iftop iotop sysstat screen curl ntp ==============="
 # - install base packages
-apt-get install -y htop iftop iotop sysstat screen curl ntp
+aptitude install -y htop iftop iotop sysstat screen curl ntp
 
 echo "================ - Enable Sysstat ==============="
 # - enable sysstat
 sed -i "s/ENABLED=\"false\"/ENABLED=\"true\"/" /etc/default/sysstat
 
-echo "================ - Add Secure User Password ==============="
-# - add secure manu password
-# echo -e 'manuL1n0de2017!' | sudo passwd manu
-
-# The end - remove script
-# rm /root/install.sh
+# echo "================ - Install Fail2Ban ==============="
+# apt-get install -y fail2ban
+#
+# echo "================ - Install Sendmail ==============="
+# apt-get install -y sendmail
+#
+# echo "================ - Configure Fail2Ban jail.local Settings ==============="
+# cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+#
+# sed -i 's/backend = auto/backend = systemd/' /etc/fail2ban/jail.local
 
 echo "================ - REBOOT ==============="
 # Reboot - kinda obvious no?
-reboot now
+reboot
