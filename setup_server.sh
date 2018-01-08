0587bbb3fb2ce8b8b01ab89f0ae9d771aa082a67
Emmanuel <emmanuel.stroem@gmail.com>
2018-01-08 21:58:35 +0800

    added command line argument for ssh_key

--- setup_server.sh
+++ setup_server.sh
@@ -10,10 +10,12 @@ if [ $# -gt 0 ]; then
     echo "Your command line contains $# arguments"
     username="$1"
     password="$2"
+    ssh_key="$3"
 else
     echo "Your command line contains no arguments"
 		username="manu"
     password=".manu@L1n0de2017!"
+    ssh_key ="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5BhQjg6WaR+0XDWPIXl8CErceSyGMhTUbZak1fA5wwvi8j2ax5CbF1CyWl7Rfd/tmz4lkPClrvhwgQiVNQ7T34en4ze1p9bZdh0BWdU2H2Cng6aUkWT+qQJNgiYR1que+ItTQcOzdzBZ2JCwxQsz1WRLSbdlnc3gXhjuGC4J2+TQk91osMcSONn/HuMRvpEGYUX+bZkatoxvwJx3Vtcl1awYHVOKa9c0js1QfNll3eUj35VUdwAIPaME56rD4QY6n3Io6iAN+yMG2bLayxEFoOIX+gkTxyw/UoMV0DW9DQmarvejTVjn8dnvnf8O7rwQLJuv2crNR1tm5f07i9QMz emmanuel@Emmanuels-MacBook-Pro.local"
 fi
 
 echo "======= Got Domain Name ============"
@@ -53,7 +55,7 @@ touch /home/$username/.ssh/authorized_keys
 
 echo " ************* - Add SSH Keys ****************"
 cat > /home/$username/.ssh/authorized_keys << EOF
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5BhQjg6WaR+0XDWPIXl8CErceSyGMhTUbZak1fA5wwvi8j2ax5CbF1CyWl7Rfd/tmz4lkPClrvhwgQiVNQ7T34en4ze1p9bZdh0BWdU2H2Cng6aUkWT+qQJNgiYR1que+ItTQcOzdzBZ2JCwxQsz1WRLSbdlnc3gXhjuGC4J2+TQk91osMcSONn/HuMRvpEGYUX+bZkatoxvwJx3Vtcl1awYHVOKa9c0js1QfNll3eUj35VUdwAIPaME56rD4QY6n3Io6iAN+yMG2bLayxEFoOIX+gkTxyw/UoMV0DW9DQmarvejTVjn8dnvnf8O7rwQLJuv2crNR1tm5f07i9QMz emmanuel@Emmanuels-MacBook-Pro.local
+$ssh_key
 EOF
 
 echo " ************* - Create User + Set Home Directory ****************"
